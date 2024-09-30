5a3bd29011ef2fecd5d17c43b9202ea30506ac28
Pietro <petervol35@gmail.com>
2024-09-30 21:29:00 +0300

    added some whitenoise

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "my_shop.urls"
@@ -120,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 MEDIA_URL = "/images/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
