3bc20c56bcd2c7b5c198554a07cc00f6e315e550
petervol27 <petervol35@gmail.com>
2024-10-08 20:06:57 +0300

    ha

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -157,3 +157,5 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+CSRF_TRUSTED_ORIGINS = ["https://products-shop-17p6.onrender.com"]
